5be4fcf013b313943438ddd2fee4dc4da94a0b0f
Quang Nguyen <quangbeo199x@gmail.com>
2022-12-08 19:42:23 +0700

    Click to set completed

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
 
 function App() {
-  const age = 23;
-  const isMale = true;
-  const student = {
-    name: 'Ununin'
-  }
-  const colorList = ['red', 'green', 'yellow']
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Xin chao {student.name} - {age} - {isMale ? 'Male' : 'Female'}
-        </p>
-        {isMale && (
+
+        {/* {isMale && (
           // React.Fragment use this when no parent
           <>
             <p>U are man</p>
@@ -26,13 +18,8 @@ function App() {
             <p>U are man</p>
           </>
         )}
-        {!isMale && <p>U are woman</p>}
+        {!isMale && <p>U are woman</p>} */}
 
-        <ul>
-          {colorList.map((color, index) => (
-            <li key={index} style={{ color }}>{color}</li>
-          ))}
-        </ul>
         <TodoFeature />
         <AlbumFeature />
       </header>
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,24 +1,39 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-  const todoList = [
+  const init = [
     {
       id: 1,
       title: "Code",
+      status: "new",
     },
     {
       id: 2,
       title: "Study",
+      status: "new",
     },
   ];
+
+  const [todoList, setTodoList] = useState(init);
+  const handleTodoClick = (todo, index) => {
+    //clone cur array
+    const newTodoList = [...todoList];
+    //toggle state
+    newTodoList[index] = {
+      ...newTodoList[index],
+      status: newTodoList[index].status === "new" ? "completed" : "new",
+    };
+    //update todo list
+    setTodoList(newTodoList);
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
